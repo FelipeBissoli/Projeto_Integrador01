986e3a8489124fcb8a35ee79e57ae54cf53f9bbc
Felipe Bissoli <felipebissoli79@gmail.com>
2024-08-15 20:18:47 -0300

    att.02
    Quadrado dos números

---
+++ atividade02.py
@@ -0,0 +1,5 @@
+'''Criar um script que entre com cinco números e imprimir o quadrado de cada número'''
+
+for i in range(5):
+    num = int(input('Digite um número: '))
+    print(f'O número digitado foi {num} e o QUADRADO dele é {num**21}')
